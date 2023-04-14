8d8902437619cf3ad8a91ca8b666cb6fccf9c3f3
fahadtufail007 <fahadtufail873@gmail.com>
2023-04-14 10:56:36 +0500

    webbee Hackathon

---
+++ App.tsx
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+
+function App(): JSX.Element {
+  const Stack = createNativeStackNavigator();
+
+  return (
+
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
+
+export default App;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to HomeScreen!</Text>
+      <Text style={styles.subtitle}>This is a simple React Native app.</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: 'gray',
+  },
+});
+
+export default HomeScreen;
